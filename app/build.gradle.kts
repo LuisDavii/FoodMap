7e43cc5ce1340ba3b0822363e50c6780433a5aee
FellypeMeneses <136759125+FellypeMeneses@users.noreply.github.com>
2025-09-24 00:56:01 -0300

    UpdateGrandle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.constraintlayout:constraintlayout:1.13.0")
 }
